f6d30d15305e34edffafa9a80c94be919bd6536e
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2026-03-25 02:20:31 +0000

    fix(ci): use formatted relative path for next-intl to satisfy turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 import path from 'node:path';
 
-const withNextIntl = createNextIntlPlugin();
+const i18nPath = './' + path.relative(process.cwd(), path.resolve(process.cwd(), 'i18n/request.ts'));
+
+const withNextIntl = createNextIntlPlugin(i18nPath);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,11 +11,6 @@ const nextConfig = {
   basePath: '/gratry-snow',
     images: {
       unoptimized: true,
-  },
-  turbopack: {
-    resolveAlias: {
-      'next-intl/config': path.resolve(process.cwd(), 'i18n/request.ts'),
-    },
   }
 };
 
