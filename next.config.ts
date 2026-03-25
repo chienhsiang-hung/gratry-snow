9db017ac19e1d423701760c99854e0a7a46156b1
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2026-03-25 02:12:22 +0000

    fix(config): update turbopack alias for next-intl in Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig = {
     images: {
       unoptimized: true,
   },
-  turbo: {
+  turbopack: {
     resolveAlias: {
       'next-intl/config': path.resolve(process.cwd(), 'i18n/request.ts'),
     },
