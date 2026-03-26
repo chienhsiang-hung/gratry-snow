3c5191b43b35319a6fab648025a56dfbdb7a2560
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2026-03-26 14:50:58 +0000

    feat: configure dynamic distDir for local and production builds
    Update `next.config.js` to set the `distDir` dynamically based on the `IS_LOCAL` environment variable. This allows local builds to output to `test/gratry-snow` for easier testing, while keeping the default `out` directory for production exports.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: process.env.IS_LOCAL === 'true' ? 'test/gratry-snow' : 'out',
   basePath: '/gratry-snow',
   images: {
     unoptimized: true,
